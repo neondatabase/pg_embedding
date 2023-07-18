e62c5d310731091b67b28703a7b34e85edc64b78
Konstantin Knizhnik <knizhnik@neon.tech>
2023-07-18 15:15:07 +0300

    Extend test for other operator classes

--- test/sql/knn.sql
+++ test/sql/knn.sql
@@ -1,7 +1,7 @@
 SET enable_seqscan = off;
 
 CREATE TABLE t (val real[]);
-INSERT INTO t (val) VALUES ('{0,0,0}'), ('{1,2,3}'), ('{1,1,1}'), (NULL);
+INSERT INTO t (val) VALUES ('{0,1,2}'), ('{1,2,3}'), ('{1,1,1}'), (NULL);
 CREATE INDEX ON t USING hnsw (val) WITH (dims=3, m=3);
 
 INSERT INTO t (val) VALUES (array[1,2,4]);
@@ -9,5 +9,19 @@ INSERT INTO t (val) VALUES (array[1,2,4]);
 explain SELECT * FROM t ORDER BY val <-> array[3,3,3];
 SELECT * FROM t ORDER BY val <-> array[3,3,3];
 SELECT COUNT(*) FROM t;
+
+CREATE INDEX ON t USING hnsw (val ann_cos_ops) WITH (dims=3, m=3);
+explain SELECT * FROM t ORDER BY val <=> array[3,3,3];
+SELECT * FROM t ORDER BY val <=> array[3,3,3];
+
+CREATE INDEX ON t USING hnsw (val ann_manhattan_ops) WITH (dims=3, m=3);
+explain SELECT * FROM t ORDER BY val <~> array[3,3,3];
+SELECT * FROM t ORDER BY val <~> array[3,3,3];
+
+SET enable_seqscan = on;
+SELECT * FROM t ORDER BY val <-> array[3,3,3];
+SELECT * FROM t ORDER BY val <=> array[3,3,3];
+SELECT * FROM t ORDER BY val <~> array[3,3,3];
+
 
 DROP TABLE t;
