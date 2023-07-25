7e5d9f60f36f28d2d8cad2ff493b77538fbbf418
Konstantin Knizhnik <knizhnik@neon.tech>
2023-07-25 15:04:34 +0300

    Fix PORTABLE_ALIGN32

--- distfunc.c
+++ distfunc.c
@@ -5,6 +5,12 @@
 #ifdef __x86_64__
 #include <immintrin.h>
 
+#if defined(__GNUC__)
+#define PORTABLE_ALIGN32 __attribute__((aligned(32)))
+#else
+#define PORTABLE_ALIGN32 __declspec(align(32))
+#endif
+
 __attribute__((target("avx2")))
 static dist_t l2_dist_impl_avx2(const coord_t *x, const coord_t *y, size_t n)
 {
