7b02ce1d3d2a74150e5d0cb811a224fefc0a6f59
Konstantin Knizhnik <knizhnik@neon.tech>
2023-07-31 17:59:31 +0300

    Rename function hnsw_handler to disk_hnsw_handler

--- embedding.c
+++ embedding.c
@@ -848,9 +848,9 @@ bool hnsw_is_deleted(label_t label)
  *
  * See https://www.postgresql.org/docs/current/index-api.html
  */
-PGDLLEXPORT PG_FUNCTION_INFO_V1(hnsw_handler);
+PGDLLEXPORT PG_FUNCTION_INFO_V1(disk_hnsw_handler);
 Datum
-hnsw_handler(PG_FUNCTION_ARGS)
+disk_hnsw_handler(PG_FUNCTION_ARGS)
 {
 	IndexAmRoutine *amroutine = makeNode(IndexAmRoutine);
 
