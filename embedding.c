f895fe5cabc7369777bc5ffccaef3a24db36a024
Alexander Bayandin <alexander@neon.tech>
2023-08-04 18:09:45 +0100

    Support Postgres 12

--- embedding.c
+++ embedding.c
@@ -108,24 +108,49 @@ _PG_init(void)
 {
 	hnsw_relopt_kind = add_reloption_kind();
 	add_int_reloption(hnsw_relopt_kind, "dims", "Number of dimensions",
-					  0, 0, INT_MAX, AccessExclusiveLock);
+					  0, 0, INT_MAX
+#if PG_VERSION_NUM >= 130000
+					  , AccessExclusiveLock
+#endif
+					  );
 	add_int_reloption(hnsw_relopt_kind, "m", "Number of neighbors of each vertex",
-					  DEFAULT_M, 0, INT_MAX, AccessExclusiveLock);
+					  DEFAULT_M, 0, INT_MAX
+#if PG_VERSION_NUM >= 130000
+					  , AccessExclusiveLock
+#endif
+					  );
 	add_int_reloption(hnsw_relopt_kind, "efconstruction", "Number of inspected neighbors during index construction",
-					  DEFAULT_EF_CONSTRUCT, 1, INT_MAX, AccessExclusiveLock);
+					  DEFAULT_EF_CONSTRUCT, 1, INT_MAX
+#if PG_VERSION_NUM >= 130000
+					  , AccessExclusiveLock
+#endif
+					  );
 	add_int_reloption(hnsw_relopt_kind, "efsearch", "Number of inspected neighbors during index search",
-					  DEFAULT_EF_SEARCH, 1, INT_MAX, AccessExclusiveLock);
+					  DEFAULT_EF_SEARCH, 1, INT_MAX
+#if PG_VERSION_NUM >= 130000
+					  , AccessExclusiveLock
+#endif
+					  );
 	hnsw_init_dist_func();
 }
 
 static void
-hnsw_build_callback(Relation index, ItemPointer tid, Datum *values,
-					bool *isnull, bool tupleIsAlive, void *state)
+hnsw_build_callback(Relation index,
+#if PG_VERSION_NUM >= 130000
+					ItemPointer tid,
+#else
+					HeapTuple hup,
+#endif
+					Datum *values, bool *isnull, bool tupleIsAlive, void *state)
 {
 	HnswIndex* hnsw = (HnswIndex*) state;
 	ArrayType* array;
 	int n_items;
 	HnswLabel u;
+
+#if PG_VERSION_NUM < 130000
+	ItemPointer tid = &hup->t_self;
+#endif
 
 	/* Skip nulls */
 	if (isnull[0])
@@ -374,10 +399,24 @@ hnsw_options(Datum reloptions, bool validate)
 		{"m", RELOPT_TYPE_INT, offsetof(HnswOptions, M)}
 	};
 
+#if PG_VERSION_NUM >= 130000
 	return (bytea *) build_reloptions(reloptions, validate,
 									  hnsw_relopt_kind,
 									  sizeof(HnswOptions),
 									  tab, lengthof(tab));
+#else
+	relopt_value *options;
+	HnswOptions *rdopts;
+	int			numoptions;
+
+	options = parseRelOptions(reloptions, validate, hnsw_relopt_kind, &numoptions);
+
+	rdopts = allocateReloptStruct(sizeof(HnswOptions), options, numoptions);
+
+	fillRelOptions((void *) rdopts, sizeof(HnswOptions), options, numoptions, validate, tab, lengthof(tab));
+
+	return (bytea *) rdopts;
+#endif
 }
 
 /*
@@ -871,7 +910,9 @@ hnsw_handler(PG_FUNCTION_ARGS)
 
 	amroutine->amstrategies = 0;
 	amroutine->amsupport = 1;
+#if PG_VERSION_NUM >= 130000
 	amroutine->amoptsprocnum = 0;
+#endif
 	amroutine->amcanorder = false;
 	amroutine->amcanorderbyop = true;
 	amroutine->amcanbackward = false;	/* can change direction mid-scan */
@@ -885,8 +926,10 @@ hnsw_handler(PG_FUNCTION_ARGS)
 	amroutine->ampredlocks = false;
 	amroutine->amcanparallel = false;
 	amroutine->amcaninclude = false;
+#if PG_VERSION_NUM >= 130000
 	amroutine->amusemaintenanceworkmem = false; /* not used during VACUUM */
 	amroutine->amparallelvacuumoptions = VACUUM_OPTION_PARALLEL_BULKDEL;
+#endif
 	amroutine->amkeytype = InvalidOid;
 
 	/* Interface functions */
