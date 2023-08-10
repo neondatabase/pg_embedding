91726b3a74d9b34edefa8044b2f005b04a7539bd
Konstantin Knizhnik <knizhnik@neon.tech>
2023-08-10 22:05:55 +0300

    Update pq test to show index size

--- test/sql/pq.sql
+++ test/sql/pq.sql
@@ -1,6 +1,6 @@
 CREATE TABLE t (id integer primary key, val real[]);
 select setseed(0.5);
-INSERT INTO t select id, array[random(), random(), random(), random()] from generate_series(1,1000) id;
+INSERT INTO t select id, array[random(), random(), random(), random()] from generate_series(1,10000) id;
 CREATE INDEX hnsw_pq_idx ON t USING hnsw (val) WITH (dims=4, m=8, efConstruction=16, efSearch=10, pqBits=3, pqSubqs=2);
 CREATE INDEX ON t USING hnsw (val) WITH (dims=4, m=8, efConstruction=16, efSearch=10);
 
@@ -12,10 +12,11 @@ SET enable_seqscan = on;
 SET enable_indexscan = off;
 explain SELECT *,val <-> array[0.5,0.5,0.5,0.5] as dist FROM t ORDER BY dist limit 10;
 SELECT *,val <-> array[0.5,0.5,0.5,0.5] as dist FROM t ORDER BY dist limit 10;
+select pg_relation_size('hnsw_pq_idx');
 
 drop index hnsw_pq_idx;
 CREATE INDEX hnsw_idx ON t USING hnsw (val) WITH (dims=4, m=8, efConstruction=16, efSearch=10);
 SELECT *,val <-> array[0.5,0.5,0.5,0.5] as dist FROM t ORDER BY dist limit 10;
-
+select pg_relation_size('hnsw_idx');
 
 DROP TABLE t;
